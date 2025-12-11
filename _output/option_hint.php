a5a8d29aa49faf516ba06d839823105d1289a1e0
Simon Hampel <simon@hampelgroup.com>
2025-12-11 11:19:36 +1100

    bump version to 1.0.1

---
+++ _output/option_hint.php
@@ -0,0 +1,21 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property string|null $hampelAccountUpgradesInfoBodyAfter Body after
+ * @property string|null $hampelAccountUpgradesInfoBodyBefore Body before
+ * @property string|null $hampelAccountUpgradesInfoTitleAfter Title after
+ * @property string|null $hampelAccountUpgradesInfoTitleBefore Title before
+ */
+class Options
+{
+}
